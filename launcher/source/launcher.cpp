d91fa47bf98769cbddcc1db8a56f7c8ebc15a89c
BonexGoo <bonexgoo@gmail.com>
2020-06-18 03:18:58 +0900

    1.1.0
    launcher 프로젝트 추가

---
+++ launcher/source/launcher.cpp
@@ -0,0 +1,328 @@
+﻿#include <boss.hpp>
+#include "launcher.hpp"
+
+#include <resource.hpp>
+#include <format/boss_bmp.hpp>
+#include <format/boss_png.hpp>
+#include <daddy.hpp>
+
+ZAY_DECLARE_VIEW_CLASS("launcherView", launcherData)
+
+ZAY_VIEW_API OnCommand(CommandType type, chars topic, id_share in, id_cloned_share* out)
+{
+}
+
+ZAY_VIEW_API OnNotify(NotifyType type, chars topic, id_share in, id_cloned_share* out)
+{
+}
+
+ZAY_VIEW_API OnGesture(GestureType type, sint32 x, sint32 y)
+{
+}
+
+static const sint32 gIconSize = 60;
+static const sint32 gButtonWidth = 100;
+static const sint32 gButtonHeight = 36;
+static const sint32 gTitleHeight = 26;
+static const sint32 gCommentHeight = 26;
+static const sint32 gWidgetHeight = 100;
+static const Color gWidgetColor[2] = {Color(96, 96, 96, 128), Color(0, 128, 255, 128)};
+
+ZAY_VIEW_API OnRender(ZayPanel& panel)
+{
+    ZAY_RGB(panel, 255, 255, 255)
+        panel.fill();
+
+    ZAY_INNER(panel, 10)
+    {
+        BOSS::String SAppsRemPath = Platform::Utility::GetProgramPath(true) + "../sapps-rem/";
+        const sint32 WidgetWidth = Math::Max(gIconSize + 300, panel.w());
+        for(sint32 i = 0, iend = m->mSApps.Count(); i < iend; ++i)
+        {
+            ZAY_XYWH(panel, 0, (gWidgetHeight + 10 + 1) * i, WidgetWidth, gWidgetHeight)
+            {
+                const bool IsInstalled = Platform::File::ExistForDir(SAppsRemPath + m->mSApps[i].mFileName);
+                ZAY_COLOR(panel, gWidgetColor[IsInstalled])
+                {
+                    ZAY_RGBA(panel, 128, 128, 128, 64)
+                        panel.fill();
+                    ZAY_RGBA(panel, 128, 128, 128, 96)
+                        panel.rect(1);
+                }
+
+                // 아이콘
+                ZAY_XYWH(panel, 5, 5, gIconSize, gIconSize)
+                    panel.stretch(m->mSApps[i].mIcon, Image::Build::AsyncNotNull);
+
+                // 정보란
+                ZAY_LTRB(panel, 5 + gIconSize + 5, 5, panel.w() - 5 - gButtonWidth - 5, panel.h() - 5)
+                {
+                    ZAY_RGBA(panel, 255, 255, 255, 64)
+                        panel.fill();
+                    ZAY_RGBA(panel, 255, 255, 255, 32)
+                        panel.rect(1);
+
+                    // 타이틀
+                    ZAY_XYWH(panel, 5, 0, panel.w() - 10, gTitleHeight)
+                    ZAY_FONT(panel, 1.2, "Arial Black")
+                    ZAY_RGB(panel, 0, 0, 0)
+                    {
+                        if(0 < m->mSApps[i].mWorkCount)
+                            panel.text(m->mSApps[i].mFileName + BOSS::String::Format(" (%d+)", m->mSApps[i].mWorkCount),
+                                UIFA_LeftTop, UIFE_Right);
+                        else panel.text(m->mSApps[i].mFileName, UIFA_LeftTop, UIFE_Right);
+                    }
+
+                    // 설명
+                    ZAY_XYWH(panel, 5, gTitleHeight, panel.w() - 10, gCommentHeight)
+                    ZAY_FONT(panel, 1, "Arial Black")
+                    ZAY_RGB(panel, 96, 96, 96)
+                        panel.text(m->mSApps[i].mReadMe, UIFA_LeftTop, UIFE_Right);
+
+                    // 실행버튼
+                    static chars CycleType[2] = {"kill", "run"};
+                    static chars CycleName[2] = {"KILL ALL", "RUN ONCE"};
+                    for(sint32 j = 0, jend = m->mSApps[i].mCycle.length(); j < jend; ++j)
+                    {
+                        const BOSS::String UIButton = BOSS::String::Format("%s-%d", CycleType[j], i);
+                        ZAY_XYWH_UI(panel, panel.w() - (110 - 5) * (j + 1), panel.h() - (35 - 5), 100, 25,
+                            (IsInstalled)? UIButton : BOSS::String(),
+                            ZAY_GESTURE_T(t, i, j)
+                            {
+                                if(t == GT_InReleased)
+                                    m->ExecSApp(i, CycleType[j]);
+                            })
+                        {
+                            const bool Focused = (panel.state(UIButton) & (PS_Focused | PS_Dragging));
+                            const bool PressMove = (panel.state(UIButton) & PS_Pressed)? 1 : 0;
+                            ZAY_MOVE(panel, PressMove, PressMove)
+                            {
+                                ZAY_COLOR(panel, gWidgetColor[IsInstalled])
+                                {
+                                    ZAY_RGBA(panel, 192, 192, 192, (Focused)? 192 : 128)
+                                        panel.fill();
+                                    ZAY_RGBA(panel, 128, 128, 128, 160)
+                                        panel.rect(1);
+                                    ZAY_RGB(panel, 128, 128, 128)
+                                    ZAY_FONT(panel, 1.0, "Arial Black")
+                                        panel.text(panel.w() / 2, panel.h() / 2 - 1, CycleName[j]);
+                                }
+                            }
+                        }
+                    }
+                }
+
+                // 버튼란
+                ZAY_LTRB(panel, panel.w() - 5 - gButtonWidth, 5, panel.w() - 5, panel.h() - 5)
+                    m->OnRenderButton(panel, i, IsInstalled);
+            }
+        }
+    }
+}
+
+launcherData::launcherData()
+{
+    BOSS::String SAppsPath = Platform::File::RootForAssetsRem() + "../sapps/";
+    Platform::File::Search(SAppsPath + '*', [](chars itemname, payload data)->void
+    {
+        auto Self = (launcherData*) data;
+        Self->AddSApp(itemname);
+    }, this, true);
+}
+
+launcherData::~launcherData()
+{
+}
+
+void launcherData::OnRenderButton(ZayPanel& panel, sint32 i, bool installed)
+{
+    // 설치버튼
+    const BOSS::String UIButton = BOSS::String::Format("button-%d", i);
+    ZAY_LTRB_UI(panel, 0, 0, gButtonWidth, gButtonHeight, UIButton,
+        ZAY_GESTURE_T(t, this, i, installed)
+        {
+            if(t == GT_InReleased)
+            {
+                if(installed)
+                    RemoveSApp(i);
+                else InstallSApp(i);
+            }
+        })
+    {
+        const bool Focused = (panel.state(UIButton) & (PS_Focused | PS_Dragging));
+        const bool PressMove = (panel.state(UIButton) & PS_Pressed)? 1 : 0;
+        ZAY_MOVE(panel, PressMove, PressMove)
+        {
+            ZAY_COLOR(panel, gWidgetColor[installed])
+            {
+                ZAY_RGBA(panel, 192, 192, 192, (Focused)? 192 : 128)
+                    panel.fill();
+                ZAY_RGBA(panel, 128, 128, 128, 160)
+                    panel.rect(1);
+            }
+
+            ZAY_FONT(panel, 1.2, "Arial Black")
+            {
+                if(installed)
+                {
+                    ZAY_RGBA(panel, 224, 0, 0, 192)
+                        panel.text(panel.w() / 2, panel.h() / 2 - 1, "REMOVE");
+                }
+                else ZAY_RGBA(panel, 0, 0, 224, 192)
+                    panel.text(panel.w() / 2, panel.h() / 2 - 1, "INSTALL");
+            }
+        }
+    }
+}
+
+buffer launcherData::PathToBuffer(chars path)
+{
+    buffer Result = nullptr;
+    if(id_file_read File = Platform::File::OpenForRead(path))
+    {
+        auto FileSize = Platform::File::Size(File);
+        Result = Buffer::Alloc(BOSS_DBG FileSize);
+        Platform::File::Read(File, (uint08*) Result, Buffer::CountOf(Result));
+        Platform::File::Close(File);
+    }
+    return Result;
+}
+
+void launcherData::AddSApp(chars path)
+{
+    auto& NewSApp = mSApps.AtAdding();
+    NewSApp.mFilePath = path;
+    NewSApp.mFileName = BOSS::String::FromWChars(Platform::File::GetShortName(WString::FromChars(NewSApp.mFilePath)));
+    NewSApp.mWorkCount = 0;
+
+    buffer NewZipBuffer = PathToBuffer(path);
+    if(NewZipBuffer)
+    {
+        sint32 FileCount = 0;
+        if(id_zip NewZip = AddOn::Zip::Create((bytes) NewZipBuffer, Buffer::CountOf(NewZipBuffer), &FileCount))
+        {
+            for(sint32 i = 0; i < FileCount; ++i)
+            {
+                bool IsDir = false;
+                chars FilePath = AddOn::Zip::GetFileInfo(NewZip, i, &IsDir);
+                if(IsDir) continue;
+
+                if(!BOSS::String::CompareNoCase(FilePath, "install/icon.png"))
+                {
+                    if(buffer NewPngBuffer = AddOn::Zip::ToFile(NewZip, i))
+                    {
+                        id_bitmap NewBmp = Png().ToBmp((bytes) NewPngBuffer, true);
+                        id_bitmap OldBmp = NewSApp.mIcon.ChangeBitmap(NewBmp);
+                        Bmp::Remove(OldBmp);
+                        Buffer::Free(NewPngBuffer);
+                    }
+                }
+                else if(!BOSS::String::CompareNoCase(FilePath, "install/readme.md"))
+                {
+                    if(buffer NewMdBuffer = AddOn::Zip::ToFile(NewZip, i))
+                    {
+                        NewSApp.mReadMe = BOSS::String(((chars) NewMdBuffer) + 3, Buffer::CountOf(NewMdBuffer) - 3);
+                        Buffer::Free(NewMdBuffer);
+                    }
+                }
+                else if(!BOSS::String::CompareNoCase(FilePath, "install/cycle.yaml"))
+                {
+                    if(buffer NewMdBuffer = AddOn::Zip::ToFile(NewZip, i))
+                    {
+                        const BOSS::String NewYaml(((chars) NewMdBuffer) + 3, Buffer::CountOf(NewMdBuffer) - 3);
+                        NewSApp.mCycle.loadYaml(dString((chars) NewYaml));
+                        Buffer::Free(NewMdBuffer);
+                    }
+                }
+            }
+            AddOn::Zip::Release(NewZip);
+        }
+        Buffer::Free(NewZipBuffer);
+    }
+}
+
+void launcherData::InstallSApp(sint32 i)
+{
+    BOSS::String SAppsRemPath = Platform::Utility::GetProgramPath(true) + "../sapps-rem/";
+    auto& CurSApp = mSApps[i];
+
+    buffer NewZipBuffer = PathToBuffer(CurSApp.mFilePath);
+    if(NewZipBuffer)
+    {
+        sint32 FileCount = 0;
+        if(id_zip NewZip = AddOn::Zip::Create((bytes) NewZipBuffer, Buffer::CountOf(NewZipBuffer), &FileCount))
+        {
+            for(sint32 i = 0; i < FileCount; ++i)
+            {
+                bool IsDir = false;
+                chars FilePath = AddOn::Zip::GetFileInfo(NewZip, i, &IsDir);
+                if(IsDir || !BOSS::String::CompareNoCase(FilePath, "install/", 8))
+                    continue;
+
+                if(buffer NewFileBuffer = AddOn::Zip::ToFile(NewZip, i))
+                {
+                    id_file NewFile = Platform::File::OpenForWrite(SAppsRemPath + CurSApp.mFileName + '/' + FilePath, true);
+                    Platform::File::Write(NewFile, (bytes) NewFileBuffer, Buffer::CountOf(NewFileBuffer));
+                    Platform::File::Close(NewFile);
+                    Buffer::Free(NewFileBuffer);
+                }
+            }
+            AddOn::Zip::Release(NewZip);
+        }
+        Buffer::Free(NewZipBuffer);
+    }
+}
+
+static void _RemoveSApp(chars dirpath)
+{
+    Platform::File::Search(dirpath, [](chars itemname, payload data)->void
+    {
+        if(Platform::File::ExistForDir(itemname))
+            _RemoveSApp(itemname);
+        else Platform::File::Remove(WString::FromChars(itemname));
+    }, nullptr, true);
+    Platform::File::RemoveDir(WString::FromChars(dirpath));
+}
+
+void launcherData::RemoveSApp(sint32 i)
+{
+    ExecSApp(i, "kill");
+    BOSS::String SAppsRemPath = Platform::Utility::GetProgramPath(true) + "../sapps-rem/";
+    _RemoveSApp(SAppsRemPath + mSApps[i].mFileName);
+}
+
+void launcherData::ExecSApp(sint32 i, chars act)
+{
+    DD_hook(mSApps[i].mCycle(dString(act)))
+    for(sint32 j = 0, jend = DD_fish.length(); j < jend; ++j)
+    {
+        auto ExecName = DD_fish[j]("exec").get();
+        if(0 < ExecName.length())
+        {
+            if(!BOSS::String::Compare(act, "kill"))
+                dDetector::killClient(ExecName, true);
+            else
+            {
+                BOSS::String ProgramPath = Platform::Utility::GetProgramPath(true);
+                #if DD_OS_LINUX
+                    ProgramPath = ProgramPath.Right(ProgramPath.Length() - 2); // "Q:"
+                #endif
+
+                dString BinPath((chars) ("../sapps-rem/" + mSApps[i].mFileName + "/bin_ubuntu/"));
+                dString WorkPath((chars) ("../sapps-rem/" + mSApps[i].mFileName + BOSS::String::Format("/work_%d/", mSApps[i].mWorkCount)));
+                auto Option = DD_fish[j]("option").get();
+                auto ArgsName = DD_fish[j]("args").get();
+                if(0 < ArgsName.length())
+                {
+                    auto DllPath = ((chars) ProgramPath) + BinPath + ArgsName;
+                    dDetector::runClient(BinPath + ExecName, Option, DllPath, ((chars) ProgramPath) + WorkPath);
+                }
+                else dDetector::runClient(BinPath + ExecName, Option, "", ((chars) ProgramPath) + WorkPath);
+            }
+        }
+    }
+
+    if(!BOSS::String::Compare(act, "kill"))
+        mSApps.At(i).mWorkCount = 0;
+    else mSApps.At(i).mWorkCount++;
+}
---
+++ launcher/source/launcher.hpp
@@ -0,0 +1,35 @@
+﻿#pragma once
+#include <service/boss_zay.hpp>
+#include <daddy.hpp>
+
+class SApp
+{
+public:
+    String mFilePath;
+    String mFileName;
+    sint32 mWorkCount;
+    Image mIcon;
+    String mReadMe;
+    dMarkup mCycle;
+};
+typedef Array<SApp> SApps;
+
+class launcherData : public ZayObject
+{
+public:
+    launcherData();
+    ~launcherData();
+
+public:
+    void OnRenderButton(ZayPanel& panel, sint32 i, bool installed);
+
+public:
+    void AddSApp(chars path);
+    void InstallSApp(sint32 i);
+    void RemoveSApp(sint32 i);
+    void ExecSApp(sint32 i, chars act);
+    buffer PathToBuffer(chars path);
+
+public:
+    SApps mSApps;
+};
